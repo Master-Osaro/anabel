e60c2522a1b8c51da899176940b19dcf773ed707
Osaro Iyoha <saro.iyoha@gmail.com>
2023-05-20 19:18:18 +0100

    Add video bg

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import Header from './components/Header'
 import anabelPhoto from './assets/Images/main_character.png';
 import magicHat from './assets/Images/magician-hat.png';
-import bgVideo from './assets/Video/land10.mp4';
+import bgVideo from './assets/Video/land10p.mp4';
 
 function App() {
 
@@ -17,17 +17,19 @@ function App() {
             <img src={magicHat} alt="" className="movie__name__magic-hat"/>
             <div className="circle_shadow"></div>
           </div>
-          
-          
-          
         </div>
         
         <div className="movie__media">
           <div className="movie__media-photo">
-            <img src={anabelPhoto} alt="" className="movie__media-photo media-photo__anabel"/>
-          </div>
-          <div className="movie__media-video">
-            <video src={bgVideo}></video>
+            <div className="photo__wrapper">
+              <img src={anabelPhoto} alt="" className="movie__media-photo media-photo__anabel"/>
+              <div className="movie__media-video">
+                <div className="video-container">
+                  <video src={bgVideo} autoPlay loop></video>
+                </div>
+              </div>
+            </div>
+            
           </div>
           <div className="movie__media-summary"></div>
         </div>
